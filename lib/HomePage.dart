7fb94ed986154dfd074147e0870466a7ef07bd95
Deepanshu <b420021@iiit-bh.ac.in>
2023-09-07 10:59:31 +0530

    Navigation button updated

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,5 +1,7 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:liquid_progress_indicator_v2/liquid_progress_indicator.dart';
+import 'package:pixel_pen/utils/ProgressIndicator.dart';
 import 'package:pixel_pen/utils/colors.dart';
 import 'package:pixel_pen/widgets/MainButton.dart';
 import 'package:file_picker/file_picker.dart';
@@ -45,6 +47,41 @@ class _HomePageState extends State<HomePage> {
       backgroundColor: AppColors.backgroundColor,
       appBar: AppBar(
         toolbarHeight: 80,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Container(
+            // height: 40, // Set the desired height
+            // width: 40, // Set the desired width
+            decoration: BoxDecoration(
+              shape: BoxShape.circle, // Use a circular shape for the button
+              color: AppColors.backgroundColor,
+              boxShadow: [
+                BoxShadow(
+                  color: AppColors.darkColor!,
+                  blurRadius: 12,
+                  offset: const Offset(4, 4),
+                  spreadRadius: 1,
+                ),
+                const BoxShadow(
+                  color: Colors.white,
+                  blurRadius: 12,
+                  offset: Offset(-4, -4),
+                  spreadRadius: 1,
+                ),
+              ],
+            ),
+            child: IconButton(
+              iconSize: 24, // Set the desired icon size
+              icon: Icon(
+                Icons.arrow_back,
+                color: AppColors.darkColor,
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        ),
         title: BounceInDown(
           delay: const Duration(milliseconds: 800),
           duration: const Duration(milliseconds: 2000),
@@ -128,6 +165,7 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   MainButton(
                     child: const Text(
@@ -150,7 +188,73 @@ class _HomePageState extends State<HomePage> {
                           color: AppColors.titleColor),
                     ),
                     onPressed: () {
-                      pickPDFFile();
+                      //TO DO Upload file
+                    },
+                  ),
+                ],
+              ),
+              // const MyProgressIndicator(progress: 0.1),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 300,
+                    height: 100 / 2,
+                    decoration: BoxDecoration(
+                        color: AppColors.backgroundColor!,
+                        borderRadius: BorderRadius.circular(8),
+                        boxShadow: [
+                          BoxShadow(
+                              color: AppColors.darkColor!,
+                              blurRadius: 12,
+                              offset: const Offset(4, 4),
+                              spreadRadius: 1),
+                          const BoxShadow(
+                              color: Colors.white,
+                              blurRadius: 12,
+                              offset: Offset(-4, -4),
+                              spreadRadius: 1),
+                        ]),
+                    child: Center(
+                      child: FadeIn(
+                        duration: Duration(seconds: 2),
+                        child: const Text(
+                          "TODO Progress Indicator",
+                          style: TextStyle(fontSize: 16),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  MainButton(
+                    child: const Text(
+                      "View word",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: AppColors.titleColor),
+                    ),
+                    onPressed: () {
+                      // TODO: View word file implementation
+                    },
+                  ),
+                  MainButton(
+                    child: const Text(
+                      "Download",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: AppColors.titleColor),
+                    ),
+                    onPressed: () {
+                      //TO DO Download file to local device
                     },
                   ),
                 ],
