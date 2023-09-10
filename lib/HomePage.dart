9613fb4656a40999a6d44f5088c58224444b1f7a
Deepanshu <b420021@iiit-bh.ac.in>
2023-09-10 12:59:16 +0530

    image processing updated

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,7 +1,8 @@
+import 'dart:io';
+
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
-import 'package:liquid_progress_indicator_v2/liquid_progress_indicator.dart';
-import 'package:pixel_pen/utils/ProgressIndicator.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:pixel_pen/utils/colors.dart';
 import 'package:pixel_pen/widgets/MainButton.dart';
 import 'package:file_picker/file_picker.dart';
@@ -17,6 +18,9 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   FilePickerResult? filePicked; // pdf file picked
   String? selectedFileName;
+  String? imagePath;
+  final textRecogniser = TextRecognizer();
+  late final outputText;
   // Function to pick a image file
   Future<void> pickPDFFile() async {
     try {
@@ -28,15 +32,42 @@ class _HomePageState extends State<HomePage> {
       if (result != null) {
         PlatformFile file = result.files.first;
         String fileName = file.name;
-
+        String? path = file.path;
+        print("BadBoy---------");
+        print(path);
         // Update the UI with the selected file name
         setState(() {
           selectedFileName = fileName;
           filePicked = result;
+          imagePath = path;
         });
       }
     } catch (e) {
       print("Error picking image : $e");
+    }
+  }
+
+  //It will take care of scanning text from image
+  Future<void> scanImage() async {
+    // if (cameraController == null) {
+    //   return;
+    // }
+    // final navigator = Navigator.of(context);
+    try {
+      // final pictureFile = await cameraController!.takePicture();
+      final file = File(imagePath!);
+      final inputImage = InputImage.fromFile(file);
+      final recognizerText = await textRecogniser.processImage(inputImage);
+      setState(() {
+        outputText = recognizerText.text;
+      });
+      // print(outputText);
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('An error occurred when scanning text'),
+        ),
+      );
     }
   }
 
@@ -155,7 +186,7 @@ class _HomePageState extends State<HomePage> {
                         duration: Duration(seconds: 2),
                         child: Text(
                           selectedFileName != null
-                              ? 'Selected PDF : $selectedFileName'
+                              ? 'Selected Image :   $selectedFileName'
                               : "No File Choosen",
                           style: const TextStyle(fontSize: 16),
                           overflow: TextOverflow.ellipsis,
@@ -198,61 +229,53 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ],
               ),
-              // const MyProgressIndicator(progress: 0.1),
-              SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 300,
-                    height: 100 / 2,
-                    decoration: BoxDecoration(
-                        color: AppColors.backgroundColor!,
-                        borderRadius: BorderRadius.circular(8),
-                        boxShadow: [
-                          BoxShadow(
-                              color: AppColors.darkColor!,
-                              blurRadius: 12,
-                              offset: const Offset(4, 4),
-                              spreadRadius: 1),
-                          const BoxShadow(
-                              color: Colors.white,
-                              blurRadius: 12,
-                              offset: Offset(-4, -4),
-                              spreadRadius: 1),
-                        ]),
-                    child: Center(
+
+              filePicked != null
+                  ? Container(
+                      height: 250, // Adjust the height as needed
+                      width: 250,
+                      decoration: BoxDecoration(
+                          color: AppColors.backgroundColor!,
+                          borderRadius: BorderRadius.circular(8),
+                          boxShadow: [
+                            BoxShadow(
+                                color: AppColors.darkColor!,
+                                blurRadius: 12,
+                                offset: const Offset(4, 4),
+                                spreadRadius: 1),
+                            const BoxShadow(
+                                color: Colors.white,
+                                blurRadius: 12,
+                                offset: Offset(-4, -4),
+                                spreadRadius: 1),
+                          ]), // Adjust the width as needed
                       child: FadeIn(
-                        duration: Duration(seconds: 2),
-                        child: const Text(
-                          "TODO Progress Indicator",
-                          style: TextStyle(fontSize: 16),
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
+                        duration: Duration(seconds: 8),
+                        child: Image.file(
+                          File(imagePath!),
                         ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+                      ))
+                  : Container(),
+              // const MyProgressIndicator(progress: 0.1),
+
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   MainButton(
                     child: const Text(
-                      "View word",
+                      "Process Me",
                       style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 18,
                           color: AppColors.titleColor),
                     ),
                     onPressed: () {
-                      // TODO: View word file implementation
+                      scanImage();
                     },
                   ),
                   MainButton(
                     child: const Text(
-                      "Download",
+                      "Copy",
                       style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 18,
@@ -264,6 +287,32 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ],
               ),
+              filePicked != null
+                  ? Container(
+                      height: 250, // Adjust the height as needed
+                      width: 250,
+                      decoration: BoxDecoration(
+                          color: AppColors.backgroundColor!,
+                          borderRadius: BorderRadius.circular(8),
+                          boxShadow: [
+                            BoxShadow(
+                                color: AppColors.darkColor!,
+                                blurRadius: 12,
+                                offset: const Offset(4, 4),
+                                spreadRadius: 1),
+                            const BoxShadow(
+                                color: Colors.white,
+                                blurRadius: 12,
+                                offset: Offset(-4, -4),
+                                spreadRadius: 1),
+                          ]), // Adjust the width as needed
+                      child: FadeIn(
+                        duration: Duration(seconds: 8),
+                        child: Image.file(
+                          File(imagePath!),
+                        ),
+                      ))
+                  : Container(),
             ],
           ),
         ),
