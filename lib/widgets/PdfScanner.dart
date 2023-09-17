d2c0d31cd8ff72e901fc2d65e00801d6c24783eb
Deepanshu <b420021@iiit-bh.ac.in>
2023-09-17 10:32:14 +0530

    PDF extracted text container updated and checked

--- lib/widgets/PdfScanner.dart
+++ lib/widgets/PdfScanner.dart
@@ -1,6 +1,9 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:pixel_pen/utils/colors.dart';
 import 'package:pixel_pen/widgets/AppBar.dart';
+import 'package:pixel_pen/widgets/MainButton.dart';
 import 'package:syncfusion_flutter_pdf/pdf.dart';
 import 'dart:io';
 
@@ -12,7 +15,7 @@ class PdfScanner extends StatefulWidget {
 }
 
 class _PdfScannerState extends State<PdfScanner> {
-  String extractedText = '';
+  String? extractedText;
 
   Future<void> extractTextFromPDF() async {
     try {
@@ -50,30 +53,89 @@ class _PdfScannerState extends State<PdfScanner> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const MyAppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                extractTextFromPDF();
-              },
-              child: Text('Select PDF and Extract Text'),
-            ),
-            SizedBox(height: 20),
-            if (extractedText.isNotEmpty)
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Text(
-                    extractedText,
-                    style: TextStyle(fontSize: 16),
-                  ),
+        appBar: const MyAppBar(),
+        backgroundColor: AppColors.backgroundColor,
+        body: SingleChildScrollView(
+            child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(height: 30),
+              MainButton(
+                child: const Text(
+                  "Press Me to Pick for PDF extraction",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      color: AppColors.titleColor),
                 ),
+                onPressed: () {
+                  extractTextFromPDF();
+                },
               ),
-          ],
-        ),
-      ),
-    );
+              extractedText != null
+                  ? Container(
+                      height: 500, // Adjust the height as needed
+                      width: 300,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 15, vertical: 20),
+                      decoration: BoxDecoration(
+                          color: AppColors.backgroundColor!,
+                          borderRadius: BorderRadius.circular(8),
+                          boxShadow: [
+                            BoxShadow(
+                                color: AppColors.darkColor!,
+                                blurRadius: 12,
+                                offset: const Offset(4, 4),
+                                spreadRadius: 1),
+                            const BoxShadow(
+                                color: Colors.white,
+                                blurRadius: 12,
+                                offset: Offset(-4, -4),
+                                spreadRadius: 1),
+                          ]), // Adjust the width as needed
+                      child: FadeIn(
+                        duration: const Duration(seconds: 8),
+                        child:
+                            SingleChildScrollView(child: Text(extractedText!)),
+                      ),
+                    )
+                  : Container(),
+              extractedText != null
+                  ? Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        MainButton(
+                          child: const Text(
+                            "Save to drive",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                                color: AppColors.titleColor),
+                          ),
+                          onPressed: () {
+                            // To Do Save to drive
+                          },
+                        ),
+                        MainButton(
+                          child: const Text(
+                            "Download",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                                color: AppColors.titleColor),
+                          ),
+                          onPressed: () {
+                            // To do Shared Prefernces
+                          },
+                        ),
+                      ],
+                    )
+                  : Container(),
+            ],
+          ),
+        )));
   }
 }
