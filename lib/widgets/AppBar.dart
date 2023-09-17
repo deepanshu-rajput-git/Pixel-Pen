869e0ce628d4e05e895e0d9e5ad5d99967e672a2
Deepanshu <b420021@iiit-bh.ac.in>
2023-09-17 09:52:53 +0530

    AppBar customized and extracted

--- lib/widgets/AppBar.dart
+++ lib/widgets/AppBar.dart
@@ -1,13 +1,12 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:pixel_pen/utils/colors.dart';
 
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
-  final String title;
   final VoidCallback? onBackButtonPressed;
 
   const MyAppBar({
     Key? key,
-    required this.title,
     this.onBackButtonPressed,
   }) : super(key: key);
 
@@ -22,7 +21,7 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
           alignment: Alignment.center,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(8),
-            shape: BoxShape.rectangle,
+            shape: BoxShape.rectangle, // Use a circular shape for the button
             color: AppColors.backgroundColor,
             boxShadow: [
               BoxShadow(
@@ -40,22 +39,48 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
             ],
           ),
           child: IconButton(
-            iconSize: 24,
+            iconSize: 24, // Set the desired icon size
             icon: Icon(
               Icons.arrow_back,
               color: AppColors.darkColor,
             ),
-            onPressed: onBackButtonPressed ?? () => Navigator.pop(context),
+            onPressed: () {
+              Navigator.pop(context);
+            },
           ),
         ),
       ),
-      title: Text(
-        title,
-        style: const TextStyle(
-          fontFamily: 'Cera Pro',
-          color: AppColors.titleColor,
-          fontSize: 35,
-          fontWeight: FontWeight.bold,
+      title: BounceInDown(
+        delay: const Duration(milliseconds: 800),
+        duration: const Duration(milliseconds: 2000),
+        child: Container(
+          decoration: BoxDecoration(
+              color: AppColors.backgroundColor!,
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: [
+                BoxShadow(
+                    color: AppColors.darkColor!,
+                    blurRadius: 12,
+                    offset: const Offset(4, 4),
+                    spreadRadius: 1),
+                const BoxShadow(
+                    color: Colors.white,
+                    blurRadius: 12,
+                    offset: Offset(-4, -4),
+                    spreadRadius: 1),
+              ]),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 2),
+            child: Text(
+              'Pixel Pen',
+              style: TextStyle(
+                fontFamily: 'Cera Pro',
+                color: AppColors.titleColor,
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
         ),
       ),
       centerTitle: true,
